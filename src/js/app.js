dd2fda642dd427211f39a58c5cc70fb2093c2e65
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-09-06 20:32:25 +0300

    changing app.js

--- src/js/app.js
+++ src/js/app.js
@@ -34,82 +34,19 @@ const arrayJSON = [
 ];
 
 createHTMl(arrayJSON, false);
-/*let tableEl = document.querySelector('.table');
 
-let stringEl = document.createElement('tr');
-
-stringEl.innerHTML = `<td class="heading">id</td>
-<td class="heading">title</td>
-<td class="heading">year</td>
-<td class="heading">imdb</td>`;
-
-tableEl.insertAdjacentElement('beforeend', stringEl);
-
-
-arrayJSON.forEach(element => {
-	stringEl = document.createElement('tr');
-	stringEl.innerHTML += `<td class="cell">${element.id}</td>
-	<td class="cell">${element.title}</td>
-	<td class="cell">${element.year}</td>
-	<td class="cell">imdb: ${element.imdb.toFixed(2)}</td>`;
-	tableEl.insertAdjacentElement('beforeend', stringEl); 
-});
-
-
-	//tableEl = document.createElement('table');
-	//tableEl2.classList.add('table');*/
-	setTimeout(() => {
-		arrayJSON.sort((a, b) => {
-			if (a.id > b.id) {
-				return 1;
-			};
-			
-			if (a.id < b.id) {
-				return -1;
-			};
-			
-			return 0;
-		});
-	
-		createHTMl(arrayJSON, true);
-	}, 15000)
-	
-
-	/*tableEl.innerHTML = '';
-
-	stringEl = document.createElement('tr');
-
-	stringEl.innerHTML = `<td class="heading">id <img class="image" src="./img/down-icon.png" alt="альтернативный текст"></td>
-	<td class="heading">title</td>
-	<td class="heading">year</td>
-	<td class="heading">imdb</td>`;
-	
-	tableEl.insertAdjacentElement('beforeend', stringEl);
+setTimeout(() => {
+	arrayJSON.sort((a, b) => {
+		if (a.id > b.id) {
+			return 1;
+		};
 		
-	arrayJSON.forEach(element => {
-		stringEl = document.createElement('tr');
-		stringEl.innerHTML += `<td class="cell">${element.id}</td>
-		<td class="cell">${element.title}</td>
-		<td class="cell">${element.year}</td>
-		<td class="cell">imdb: ${element.imdb.toFixed(2)}</td>`;
-		tableEl.insertAdjacentElement('beforeend', stringEl); 
+		if (a.id < b.id) {
+			return -1;
+		};
+		
+		return 0;
 	});
-	
-arrayJSON.sort((a, b) => {
-	if (a.id > b.id) {
-    return 1;
-	};
-	
-  if (a.id < b.id) {
-    return -1;
-	};
-	
-  return 0;
-});
 
-const tableEl = document.createElement('table');
-
-arrayJSON.forEach(element => {
-	let el = document.createElement('tr');
-	el.classList.add('')
-})*/
+	createHTMl(arrayJSON, true);
+}, 15000);
